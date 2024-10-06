c7ffe292d3555d5863f3c0b795fb2a0d56526313
xl-24 <xi_li72@hotmail.com>
2024-10-06 11:13:44 -0500

    Updated

--- hf_asr_python_xl.py
+++ hf_asr_python_xl.py
@@ -4,40 +4,39 @@
 # This is also a place for me to learn python
 # XL: 10/6/2024
 #-----------------------------------------------------------------------------
-
-from datasets import load_dataset
-import matplotlib.pyplot as plt
-
-dataset = load_dataset(
-    "hf-internal-testing/librispeech_asr_dummy", "clean", split="validation"
-)
-
-print(dataset)
-
-
-from IPython.display import Audio
-
-sample = dataset[0]
-data_out = sample["audio"]["array"]
-
-print(data_out)
-plt.plot(data_out)
-plt.show()
-
-
-
-#print(sample)
-print(sample["text"])
-Audio(sample["audio"]["array"], rate=sample["audio"]["sampling_rate"])
-
 import torch
+from datasets import load_dataset
+from IPython.display import Audio
+import matplotlib.pyplot as plt
 from transformers import pipeline
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-pipe = pipeline(
-    "automatic-speech-recognition", model="openai/whisper-base", device=device
-)
+# XL: load dataset
+dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+print(dataset)
 
-out_text = pipe(sample["audio"], max_new_tokens=256)
-print("---final output---\n")
-print(out_text['text'])
+# XL: show dataset
+sample = dataset[0]
+data_out = sample["audio"]["array"]
+rate_out = sample["audio"]["sampling_rate"]
+
+print(data_out)
+print(rate_out)
+
+# plt.plot(data_out)
+# plt.show()
+
+
+
+# #print(sample)
+# print(sample["text"])
+Audio(sample["audio"]["array"], rate=sample["audio"]["sampling_rate"])
+
+
+# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# pipe = pipeline(
+#     "automatic-speech-recognition", model="openai/whisper-base", device=device
+# )
+
+# out_text = pipe(sample["audio"], max_new_tokens=256)
+# print("---final output---\n")
+# print(out_text['text'])
